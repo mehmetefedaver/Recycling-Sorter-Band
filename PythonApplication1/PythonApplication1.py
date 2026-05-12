0cc20d2d693990e81a6249ebb288fc481b513e2f
Mehmet Efe Daver <mehmetefedaver@gmail.com>
2026-05-12 12:44:11 +0300

    PythonApplication1.py

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -22,7 +22,7 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     if not ret: 
-        print("Kamera okunamadı kanka, kabloyu falan kontrol et!")
+        print("Kamera okunamadı")
         break
 
     
@@ -37,7 +37,7 @@ while True:
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.putText(frame, etiket, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 
-    cv2.imshow("Kanka TFLite Geri Donusum Testi", frame)
+    cv2.imshow("Recycling-Sorter-Band", frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
